dfa49a67b8965fc4fd7721b72e2af3282104c304
bob9952 <thegreatsubotic@gmail.com>
2024-01-04 00:24:31 +0100

    Ref: clang-tidy change to be generated from qmake and bear, then piped to run-clang-tidy

--- clang-tidy/clang-tidy.sh
+++ clang-tidy/clang-tidy.sh
@@ -1,6 +1,16 @@
-CURRENT_DIR=$(pwd)
-PROJECT_DIR="${CURRENT_DIR}/../stock-app/"
-BUILD_DIR="${PROJECT_DIR}/build-source-Desktop_Qt_6_5_3_GCC_64bit-Debug/"
+QMAKE=/home/bob9952/Qt/6.5.3/gcc_64/bin/qmake
+
+mkdir ../stock-app/source/build && cd ../stock-app/source/build
+${QMAKE} -spec linux-clang ../source.pro
+make qmake_all 
+bear -- make > /dev/null # generates compile_commands.json
+
+cd ../../../clang-tidy
+
+CURRENT_DIR=$(pwd) # source
+PROJECT_DIR="${CURRENT_DIR}/../stock-app"
+BUILD_DIR="${PROJECT_DIR}/source/build/"
 
 run-clang-tidy -p "${BUILD_DIR}" -header-filter='${PROJECT_DIR}/source/.*' $(find '${PROJECT_DIR}/source/' \( -name "*.cpp" -or -name "*.hpp" \)) > out.txt
 
+rm -rf ../stock-app/source/build
