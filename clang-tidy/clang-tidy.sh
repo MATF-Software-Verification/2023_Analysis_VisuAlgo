24fc196bbb31a8501f3c3b3b1cb65d973ea17773
bob9952 <thegreatsubotic@gmail.com>
2024-01-05 01:14:58 +0100

    Merge

--- clang-tidy/clang-tidy.sh
+++ clang-tidy/clang-tidy.sh
@@ -1,21 +1,5 @@
 QMAKE=/home/bob9952/Qt/6.5.3/gcc_64/bin/qmake
 
-<<<<<<< HEAD
-mkdir ../stock-app/source/build && cd ../stock-app/source/build
-${QMAKE} -spec linux-clang ../source.pro
-make qmake_all 
-bear -- make > /dev/null # generates compile_commands.json
-
-cd ../../../clang-tidy
-
-CURRENT_DIR=$(pwd) # source
-PROJECT_DIR="${CURRENT_DIR}/../stock-app"
-BUILD_DIR="${PROJECT_DIR}/source/build/"
-
-run-clang-tidy -p "${BUILD_DIR}" -header-filter='${PROJECT_DIR}/source/.*' $(find '${PROJECT_DIR}/source/' \( -name "*.cpp" -or -name "*.hpp" \)) > out.txt
-
-rm -rf ../stock-app/source/build
-=======
 mkdir -p ../VisuAlgo/visuAlgo/build && cd ../VisuAlgo/visuAlgo/build
 ${QMAKE} CONFIG+=debug -spec linux-clang ../visuAlgo.pro
 make qmake_all 
@@ -26,5 +10,3 @@ cd ../../../clang-tidy
 run-clang-tidy -p ../VisuAlgo/visuAlgo/build/ -header-filter='../VisuAlgo/visuAlgo/Headers/.*' ../VisuAlgo/visuAlgo/Resources/ ../visuAlgo/Headers/ > out.txt
 
 rm -rf ../VisuAlgo/visuAlgo/build
-
->>>>>>> 6f191c5 (Added clang-tidy script and output)
